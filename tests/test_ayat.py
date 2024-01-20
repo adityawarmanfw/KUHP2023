777e798e3a085df627b9a1fca65e25f5c143023f
edo-showwcase <adityawarmanfw@gmail.com>
2024-01-20 20:29:06 +0700

    tambah test

--- tests/test_ayat.py
+++ tests/test_ayat.py
@@ -7,10 +7,16 @@ con.sql("""
             FROM read_csv_auto('././KUHP - AYAT.tsv', sep='\t')
         );
         """)
+def test_pasal_tidak_null():
+    """
+    Pastikan nilai dalam kolom 'pasal' tidak null.
+    """
+    result = con.sql("SELECT bool_and(pasal IS NOT NULL) FROM kuhp_ayat;").fetchone()[0]
+    assert result == True
 
 def test_ayat_lebih_dari_sama_dengan_nol():
     """
-    Cek nilai dalam kolom 'ayat' lebih dari atau sama dengan nol.
+    Pastikan nilai dalam kolom 'ayat' lebih dari atau sama dengan nol.
     """
     result = con.sql("SELECT bool_and(ayat >= 0) FROM kuhp_ayat;").fetchone()[0]
     assert result == True
